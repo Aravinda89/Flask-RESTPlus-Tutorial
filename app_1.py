7e1da1ba675c3fe24de7fb06b18b513f6aa9f43e
aravindagayan <1513223865@qq.com>
2019-08-07 00:44:34 +0800

    post

--- app_1.py
+++ app_1.py
@@ -1,13 +1,15 @@
 # App 1
 
 from flask import Flask
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, fields
 
 app = Flask(__name__)
 api = Api(app)
 
+a_language = api.model('Language', {'language': fields.String('The language.')})
+
 languages = []
-python = {'language' : 'Python'}
+python = {'language': 'Python'}
 languages.append(python)
 
 
@@ -16,8 +18,14 @@ class Language(Resource):
 	def get(self):
 		return languages
 
+	@api.expect(a_language)
+	def post(self):
+		languages.append(api.payload)
+		return {'result': 'Language added'}, 201
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
 
 
