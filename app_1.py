dadc99da255dbafe6c59ab79c475b723cf0955d5
aravindagayan <1513223865@qq.com>
2019-08-07 00:32:36 +0800

    get update

--- app_1.py
+++ app_1.py
@@ -6,13 +6,18 @@ from flask_restplus import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+languages = []
+python = {'language' : 'Python'}
+languages.append(python)
+
 
 @api.route('/language')
 class Language(Resource):
 	def get(self):
-		return {'hey': 'there'}
+		return languages
 
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
 
