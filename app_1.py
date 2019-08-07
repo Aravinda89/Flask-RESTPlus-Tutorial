046851fc7410cb3d107617756a2a47e818fda721
y.ga <y.ga@controlexpert.com>
2019-08-07 11:51:12 +0800

    Swagger UI

--- app_1.py
+++ app_1.py
@@ -1,10 +1,18 @@
 # App 1
 
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restplus import Api, Resource, fields
 
 app = Flask(__name__)
-api = Api(app)
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint, doc='/documentation')
+
+app.register_blueprint(blueprint)
+
+# api = Api(app, doc=False)
+# api = Api(app)
+
+# app.config['SWAGGER_UI_JSONEDITOR'] = True
 
 # a_language = api.model('Language', {'language': fields.String('The language.'), 'id': fields.Integer('ID')})
 a_language = api.model('Language', {'language': fields.String('The language.')})
